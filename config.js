af13fc4cd1a6f239dc2f14a09c09336801bface3
Erick Martínez <erickmartinez.dev@gmail.com>
2019-10-18 23:57:59 -0600

    Configuración

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {};
+
+config.api = "http://localhost:3000/api";
+config.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFwcDAxIiwibmFtZSI6IlNlbmQtTWFpbCIsInN0YXRlIjoiQSIsImlhdCI6MTU3MTQ2NDUzNCwiZXhwIjoxNTcxNTUwOTM0fQ.ugn1gnNgGha88OqLoeevJ_E-ZKgGzv8_n87bA1Qzw9w"
+config.time = 10000 //10seg
+
+module.exports = config;
